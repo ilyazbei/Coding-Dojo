cf96a3b6961079a760f2c8c20b5fede77247c08b
ilyaz <ilyazbei@gmail.com>
2017-04-03 17:59:38 -0700

    first day c#

---
+++ dotnet/boxing_unboxing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing_unboxing
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create an empty List of type object
+            List<object> myList = new List<object>();
+
+            // Add the following values to the list: 7, 28, -1, true, "chair"
+            myList.Add(7);
+            myList.Add(28);
+            myList.Add(-1);
+            myList.Add(true);
+            myList.Add("chair");
+
+            //Loop through the list, display all values and Add all values that are Int type together and output the sum
+            int sum = 0;
+            foreach(var obj in myList) {
+                Console.WriteLine(obj);
+                if(obj is int) {
+                    sum += (int)obj;
+                }
+            }
+            Console.WriteLine("The sum of all the integers in the list is {0}", sum);
+        }
+    }
+}
---
+++ dotnet/cs_fund_2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cs_fund_2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Three Basic Arrays
+            int[] valArray = new int[10] {0,1,2,3,4,5,6,7,8,9};
+            // Console.WriteLine(valArray);
+            string[] nameArray = new string[] {"Tim", "Martin", "Nikki", "Sara"};
+            // Console.WriteLine(nameArray);
+            bool[] boolArray = new bool[10];
+            for(int i = 0; i < 10; i += 2)
+            {
+                boolArray[i] = true;
+                // Console.WriteLine(boolArray);
+            }
+
+
+            // Multiplication Table
+            int[,] mult = new int[10,10];
+            for(int x = 0; x < 10; x++)
+            {
+                for(int y = 0; y < 10; y++)
+                {
+                    mult[x, y] = (x + 1) * (y + 1);
+                    // Console.WriteLine(mult[x,y]);
+                }
+            }
+
+            //List of Ice Cream Flavors
+            List<string> flavors = new List<string>();
+            flavors.Add("Chocolate");
+            flavors.Add("Vanilla");
+            flavors.Add("Bananna");
+            flavors.Add("Cookie Crunch");
+            flavors.Add("Strawbery");
+            flavors.Add("Orange"); 
+            flavors.Add("Butter"); 
+            flavors.Add("WheapCream");
+            flavors.Add("Something");
+            flavors.Add("Anything");    
+
+            //Output the length of the List
+            Console.WriteLine(flavors.Count);
+
+            //Print the 3rd value then remove it
+            Console.WriteLine("The third flavor in the list is: " + flavors[2]);
+            flavors.RemoveAt(2);
+
+            // Output the updated length of the List of flavors
+            Console.WriteLine(flavors.Count);
+
+
+
+            // User Info Dictionary
+            Dictionary<string, string> userInfo = new Dictionary<string,string>();
+            Random rand = new Random();
+            foreach(string name in nameArray)
+            {
+                userInfo[name] = flavors[rand.Next(flavors.Count)];
+            }
+
+            //Looping through info Dictionary
+            Console.WriteLine("Users and their favorite ice cream flavors:");
+            foreach(KeyValuePair<string, string> info in userInfo)
+            {
+                Console.WriteLine(info.Key + " - " + info.Value);
+            }
+
+        } 
+
+    }
+
+}
